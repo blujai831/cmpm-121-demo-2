88ba6b7c4d7b0b9e346a596b853eda00eb3f7aa7
blujai831 <webmaster@blujai831.dev>
2024-10-15 09:09:04 -0700

    Add h1 element.

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,14 @@ const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+function makeElement<Tag extends keyof HTMLElementTagNameMap>(
+    what: Tag, how?: (elem: HTMLElementTagNameMap[Tag]) => void
+): HTMLElementTagNameMap[Tag] {
+    const elem = document.createElement(what);
+    how?.call(elem, elem);
+    app.appendChild(elem);
+    return elem;
+}
+
+makeElement('h1', elem => elem.innerHTML = APP_NAME);
