703f960ee02e7cf5b6e5983abae9a25e8a52408a
blujai831 <webmaster@blujai831.dev>
2024-10-15 10:10:03 -0700

    Allow user to draw on the canvas with a pointing device

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import "./style.css";
 
 const APP_NAME = "Sticker Sketchpad";
+const LEFT_CLICK = 1; // Why no standard library enum?
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
@@ -15,7 +16,31 @@ function makeElement<Tag extends keyof HTMLElementTagNameMap>(
 }
 
 makeElement('h1', elem => elem.innerHTML = APP_NAME);
-makeElement('canvas', elem => {
+const canvas = makeElement('canvas', elem => {
     elem.width = 256;
     elem.height = 256;
 });
+
+const canvasContext: CanvasRenderingContext2D = (() => {
+    const result = canvas.getContext('2d');
+    if (result === null) throw Error("No 2D rendering support");
+    else return result;
+})();
+canvasContext.lineWidth = 4;
+canvasContext.strokeStyle = 'black';
+
+canvas.addEventListener('mousemove', ev => {
+    if ((ev.buttons & LEFT_CLICK) == LEFT_CLICK) {
+        canvasContext.beginPath();
+        canvasContext.moveTo(
+            ev.clientX - ev.movementX - canvas.offsetLeft,
+            ev.clientY - ev.movementY - canvas.offsetTop
+        );
+        canvasContext.lineTo(
+            ev.clientX - canvas.offsetLeft,
+            ev.clientY - canvas.offsetTop
+        );
+        canvasContext.closePath();
+        canvasContext.stroke();
+    }
+})
