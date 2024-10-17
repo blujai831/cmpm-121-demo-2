cd03f52fec3ae3dbdc9c93a5775d597ef9a10ea4
blujai831 <webmaster@blujai831.dev>
2024-10-17 15:34:10 -0700

    Add buttons to select between thin and thick marker style. This suffices to satisfy step 6 as prescribed by the school assignment for which purpose this fork is maintained.

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ const APP_NAME = "Sticker Sketchpad" as const;
 const LEFT_CLICK = 0 as const;
 const LEFT_CLICK_FLAG = 1 as const;
 
+const DRAWING_TOOLS = {
+    "Thin Marker": (posn: Point) =>
+        new DrawStrokeCommand(posn, 2),
+    "Thick Marker": (posn: Point) =>
+        new DrawStrokeCommand(posn, 6)
+} as const;
+
 // Interfaces
 
 interface Point {x: number, y: number}
@@ -14,16 +21,23 @@ interface DrawCommand {
     display(ctx: CanvasRenderingContext2D): void;
 }
 
-// Dynamic constants
+// Dynamic globals
 
 const app = document.querySelector<HTMLDivElement>("#app")!;
 const displayList: DrawCommand[] = [];
 const undoStack = displayList;
 const redoStack: DrawCommand[] = [];
+let drawingTool: keyof typeof DRAWING_TOOLS;
 
-// UI output
+// Utility functions
 
-document.title = APP_NAME;
+/* Technique learned from:
+https://www.totaltypescript.com/iterate-over-object-keys-in-typescript */
+function keysAsUnion<T extends object>(obj: T): (keyof T)[] {
+    const result: (keyof T)[] = [];
+    for (const key in obj) result.push(key);
+    return result;
+}
 
 function makeElement<Tag extends keyof HTMLElementTagNameMap>(
     parent: Node, what: Tag, attrs?: Partial<HTMLElementTagNameMap[Tag]>,
@@ -36,10 +50,15 @@ function makeElement<Tag extends keyof HTMLElementTagNameMap>(
     return elem;
 }
 
+// UI output
+
+document.title = APP_NAME;
+
 makeElement(app, 'h1', {}, elem => elem.innerHTML = APP_NAME);
 const canvas = makeElement(app, 'canvas', {
     id: 'user-drawing-area', width: 256, height: 256
 });
+const toolButtonsDiv = makeElement(app, 'div', {id: 'tool-buttons'});
 
 // App implementation
 
@@ -48,8 +67,6 @@ const canvasContext: CanvasRenderingContext2D = (() => {
     if (result === null) throw Error("No 2D rendering support");
     else return result;
 })();
-canvasContext.lineWidth = 4;
-canvasContext.strokeStyle = 'black';
 
 function drawingUndo(): boolean {
     if (undoStack.length > 0) {
@@ -73,15 +90,22 @@ function drawingClear(): void {
     canvas.dispatchEvent(new Event('drawing-changed'));
 }
 
-function drawingBeginUndoStep(command: DrawCommand): void {
+function drawingBeginUndoStep(posn: Point): void {
     redoStack.length = 0;
-    undoStack.push(command);
+    undoStack.push(DRAWING_TOOLS[drawingTool](posn));
+}
+
+function drawingSetTool(which: keyof typeof DRAWING_TOOLS): void {
+    drawingTool = which;
+    toolButtonsDiv.dispatchEvent(new Event('tool-changed'));
 }
 
 class DrawStrokeCommand implements DrawCommand {
     private points: Point[];
-    public constructor(start: Point) {
+    private lineWidth: number;
+    public constructor(start: Point, lineWidth: number) {
         this.points = [start];
+        this.lineWidth = lineWidth;
     }
     public drag(where: Point): void {
         this.points.push(where);
@@ -90,6 +114,7 @@ class DrawStrokeCommand implements DrawCommand {
         let lastPosn: Point | null = null;
         for (const posn of this.points) {
             if (lastPosn !== null) {
+                ctx.lineWidth = this.lineWidth;
                 ctx.beginPath();
                 ctx.moveTo(lastPosn.x, lastPosn.y);
                 ctx.lineTo(posn.x, posn.y);
@@ -103,16 +128,25 @@ class DrawStrokeCommand implements DrawCommand {
 
 // UI input
 
+for (const toolName of keysAsUnion(DRAWING_TOOLS)) {
+    makeElement(toolButtonsDiv, 'button', {
+        innerHTML: toolName,
+        onclick: _ => drawingSetTool(toolName)
+    }, elem => toolButtonsDiv.addEventListener('tool-changed', _ => {
+        elem.disabled = (drawingTool == toolName);
+    }, true));
+}
+
 makeElement(app, 'button', {innerHTML: "Undo", onclick: drawingUndo});
 makeElement(app, 'button', {innerHTML: "Redo", onclick: drawingRedo});
 makeElement(app, 'button', {innerHTML: "Clear", onclick: drawingClear});
 
 canvas.addEventListener('mousedown', ev => {
     if (ev.button == LEFT_CLICK) {
-        drawingBeginUndoStep(new DrawStrokeCommand({
+        drawingBeginUndoStep({
             x: ev.clientX - canvas.offsetLeft,
             y: ev.clientY - canvas.offsetTop
-        }));
+        });
         canvas.dispatchEvent(new Event('drawing-changed'));
     }
 });
@@ -137,3 +171,8 @@ canvas.addEventListener('drawing-changed', _ => {
     canvasContext.clearRect(0, 0, canvas.width, canvas.height);
     for (const command of displayList) command.display(canvasContext);
 });
+
+// Runtime initialization
+
+drawingSetTool("Thin Marker");
+canvasContext.strokeStyle = 'black';
